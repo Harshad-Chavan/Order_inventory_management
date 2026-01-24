17762673e9a6d4469a9fab694113df485c5a4f58
harshad <harshadchavan141@gmail.com>
2026-01-24 06:56:19 +0530

    Added column in user model

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from app.api.routes import health_check
+from app.api.routes import health_check, user, auth
 from app.core.settings import settings
 
 app = FastAPI(title=settings.APP_NAME, version="1.0.0")
 
 app.include_router(health_check.router, prefix="/health_check", tags=["health_check"])
+app.include_router(user.router, prefix="/user", tags=["users"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8001)
--- app/models/user.py
+++ app/models/user.py
@@ -6,8 +6,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False, index=True)
     email = Column(String(255), unique=True, nullable=False, index=True)
-    hashed_password = Column(String(255), nullable=False)
+    hashed_password = Column(String, nullable=False)
 
     is_active = Column(Boolean, default=True)
     is_admin = Column(Boolean, default=False)
